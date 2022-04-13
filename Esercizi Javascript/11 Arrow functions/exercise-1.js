aa496dc627e1f393eebc80bdbb8813c520de82fa
Amalia <cicala.amalia@gmail.com>
2022-04-13 15:47:10 +0200

    feat: aggiunto es. Javascript 11 Arrow functions

---
+++ Esercizi Javascript/11 Arrow functions/exercise-1.js
@@ -0,0 +1,13 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+const sum = (a, b) => a + b
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+const log = value => console.log(value)
+
+log(sum(2, 5));
