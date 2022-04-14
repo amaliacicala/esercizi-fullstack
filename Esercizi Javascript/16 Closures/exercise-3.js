09ef2e31103042d01e48edd8e97d2605aaa7ddc6
Amalia <cicala.amalia@gmail.com>
2022-04-14 12:15:05 +0200

    feat: aggiunto es. Javascript 16 Closures

---
+++ Esercizi Javascript/16 Closures/exercise-3.js
@@ -0,0 +1,22 @@
+function createStore() {
+  let inventory = []
+
+  return function(item) {
+    inventory.push(item)
+    console.log(inventory)
+  }
+
+}
+
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat)
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
