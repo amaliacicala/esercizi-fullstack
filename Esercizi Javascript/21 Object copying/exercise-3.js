6c35e26336d8c23e1a489a78053d677c4ec70d4f
Amalia <cicala.amalia@gmail.com>
2022-04-22 21:23:08 +0200

    feat: aggiunto es. Javascript 21 Object copying

---
+++ Esercizi Javascript/21 Object copying/exercise-3.js
@@ -0,0 +1,22 @@
+const person1 = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+  address: {
+    state: 'Italy',
+    region: 'Lazio',
+    city: 'Rome'
+  }
+};
+
+// Argh! Nonostante abbia utilizzato Object.assign(), modificando la proprietà "address",
+// viene modificato il riferimento all'oggetto person1
+// const person2 = Object.assign({}, person1);
+
+const person2 = JSON.parse(JSON.stringify(person1));
+
+person2.address.region = 'Lombardia';
+person2.address.city = 'Milan';
+
+console.log(person1);
+console.log(person2);
