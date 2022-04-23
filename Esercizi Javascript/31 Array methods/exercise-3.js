8db3d67797aaede7abe9a5cd37aa4fe7410ab70d
Amalia <cicala.amalia@gmail.com>
2022-04-23 17:27:44 +0200

    feat: aggiunto es. Javascript 31 Array methods

---
+++ Esercizi Javascript/31 Array methods/exercise-3.js
@@ -0,0 +1,22 @@
+function calculateAverageAge(persons) {
+  let totalAges = persons.reduce((n, {age}) => n + age, 0);
+  let averageAge = totalAges / persons.length;
+  return averageAge
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
