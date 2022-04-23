df2e6e2db9ffaaa50ab0e5d03aa18ae1813427d2
Amalia <cicala.amalia@gmail.com>
2022-04-23 15:40:27 +0200

    feat: aggiunto es. Javascript 28 Optional chaining

---
+++ Esercizi Javascript/28 Optional chaining/exercise-2.js
@@ -0,0 +1,9 @@
+const order = {};
+
+// if (order && order.customer && order.customer.address && !order.customer.address.city) {
+//   console.log('City is required');
+// }
+
+if (!order.customer?.address?.city) {
+  console.log('City is required')
+}
