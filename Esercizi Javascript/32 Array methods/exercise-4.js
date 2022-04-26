38df2297c42236ee24378d7891685226d4eab70c
Amalia <cicala.amalia@gmail.com>
2022-04-26 20:00:29 +0200

    feat: aggiunto es. Javascript 32 Array methods

---
+++ Esercizi Javascript/32 Array methods/exercise-4.js
@@ -0,0 +1,57 @@
+function uncompletedNotes(notes) {
+  return notes.filter((notes) => notes.todos.some((todos) => todos.done === false)
+)}
+
+const notes = [
+  {
+    id: 1,
+    description: 'Workout program',
+    todos: [
+      {
+        id: 1,
+        name: 'Push ups - 10 x 3',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'Abdominals - 20 x 3',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Tapis Roulant - 15min',
+        done: true
+      }
+    ]
+  },
+  {
+    id: 2,
+    description: 'Front-end Roadmap',
+    todos: [
+      {
+        id: 1,
+        name: 'Learn HTML',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Learn CSS',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Learn JavaScript',
+        done: true
+      },
+      {
+        id: 4,
+        name: 'Learn Angular',
+        done: true
+      }
+    ]
+  }
+]
+
+const notesInProgress = uncompletedNotes(notes);
+console.log('All notes: ', notes);
+console.log('Notes In Progress: ', notesInProgress);
