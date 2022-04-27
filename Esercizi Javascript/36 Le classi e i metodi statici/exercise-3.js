95fe8ee153e61a4260febc5b90e8618c9f36120e
Amalia <cicala.amalia@gmail.com>
2022-04-27 12:25:57 +0200

    feat: aggiunto es. Javascript 36 Le classi e i metodi statici

---
+++ Esercizi Javascript/36 Le classi e i metodi statici/exercise-3.js
@@ -0,0 +1,18 @@
+class Person {
+  static fromObject(obj) {
+    return new Person(obj.firstName, obj.lastName);
+  }
+
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const obj = {
+  firstName: 'Mario',
+  lastName: 'Rossi'
+};
+
+const person = Person.fromObject(obj);
+console.log(`${person.firstName} ${person.lastName}`);
