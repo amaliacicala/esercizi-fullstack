bbf1320c823bbde80b78b76ea249da0ef6eb53f1
Amalia <cicala.amalia@gmail.com>
2022-05-03 18:47:19 +0200

    feat: aggiunto es. Javascript 49 JSON methods

---
+++ Esercizi Javascript/49 JSON methods/exercise-1.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+
+// Print developer as json object
+
+let json = JSON.stringify(developer);
+console.log(typeof json);
+console.log(json);
