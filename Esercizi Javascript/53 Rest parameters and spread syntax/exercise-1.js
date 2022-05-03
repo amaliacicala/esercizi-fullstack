ce6e2a0a2c6e411eb33d5862d27071da0fa845a7
Amalia <cicala.amalia@gmail.com>
2022-05-03 20:12:34 +0200

    feat: aggiunto es. Javascript 53 Rest parameters and spread syntax

---
+++ Esercizi Javascript/53 Rest parameters and spread syntax/exercise-1.js
@@ -0,0 +1,10 @@
+// function sum(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+
+function sum(...numbers) {
+    let total = numbers.reduce((a,b) => a + b);
+    return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
