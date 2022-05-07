afa12ceb58d016edc915b8ab144a13d0ca0cb2ca
Amalia <cicala.amalia@gmail.com>
2022-05-07 11:02:45 +0200

    feat: corretto es. Javascript 45 Destructuring assignment

--- Esercizi Javascript/45 Destructuring assignment/exercise-2.js
+++ Esercizi Javascript/45 Destructuring assignment/exercise-2.js
@@ -1,16 +1,15 @@
-// const person = {
-//   id: 1,
-//   firstName: 'Mario',
-//   lastName: 'Rossi',
-//   age: 25
-// };
+const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+};
 
-const person = {id: 1, firstName: 'Mario', lastName: 'Rossi', age: 25};
+// const id = person.id;
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
 
-const id = person.id;
-const firstName = person.firstName;
-const lastName = person.lastName;
-const age = person.age;
-
-
+const { id = person.id, firstName = person.firstName, lastName = person.lastName, age = person.age } = person
+  
 console.log(id, firstName, lastName, age);
