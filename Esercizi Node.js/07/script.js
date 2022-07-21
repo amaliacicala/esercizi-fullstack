530864b8a5ae9e40873a3677a8c3374187f9b7ee
Amalia <cicala.amalia@gmail.com>
2022-07-21 17:00:05 +0200

    feat: add ex. Node 07

---
+++ Esercizi Node.js/07/script.js
@@ -0,0 +1,16 @@
+const figlet = require('figlet');
+
+figlet.text('Meow!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
