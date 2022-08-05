67460104068132262882e92bbdb3ea8f22c1aec8
Amalia Cicala <cicala.amalia@gmail.com>
2022-08-05 20:21:17 +0200

    rename 15-17 folder to API-ex

--- Esercizi Node.js/15-17/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	verbose: true,
-	clearMocks: true,
-	setupFilesAfterEnv: ['./src/lib/prisma/client.mock.ts'],
-};
--- Esercizi Node.js/15-17/src/server.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import app from './app';
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-	console.log(`Server's running at http://localhost:${port} 🏃`);
-});
